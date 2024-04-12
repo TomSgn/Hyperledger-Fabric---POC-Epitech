1fe8196f468d670ec81578faa6925d12f3106e18
TomSgn <tom.seiguin@epitech.eu>
2024-04-12 16:35:20 +0200

    {Add}: Init Backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,30 +1,13 @@
+from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseModel
-from xrpl.account import get_balance
-from xrpl.clients import JsonRpcClient
-from xrpl.models import Payment, Tx
-from xrpl.transaction import submit_and_wait
-from xrpl.wallet import generate_faucet_wallet
 
 app = FastAPI()
 
-client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
-
-# Create two wallets to send money between on the test network
-wallet1 = generate_faucet_wallet(client, debug=True)
-wallet2 = generate_faucet_wallet(client, debug=True)
-
-
-class Wallet(BaseModel):
-    adress: str
-    price: float
-
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Hello": "World"}
 
 
-@app.get("/wallet")
-async def wallet():
-    return {"message": "Walletttt"}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
